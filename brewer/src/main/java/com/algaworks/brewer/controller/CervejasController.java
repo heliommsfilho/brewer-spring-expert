9e31356702fbc896e1cc36f711da72d289b09abe
Hélio Márcio Filho <heliommsfilho@outlook.com>
2018-06-13 17:08:04 -0300

    3.7. Introdução a validação do formulário

--- brewer/src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ brewer/src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -1,6 +1,9 @@
 package com.algaworks.brewer.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -15,8 +18,14 @@ public class CervejasController {
 	}
 	
 	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
-	public String cadastrar(Cerveja cerveja) {
+	public String cadastrar(@Valid Cerveja cerveja, BindingResult result) {
+		
+		if (result.hasErrors()) {
+			System.out.println("Erro de validação");
+		}
+		
 		System.out.println("Chamou Cadastrar ---> " + cerveja.getSku());
 		return "cerveja/CadastroCerveja";
 	}
 }
+ 
--- brewer/src/main/java/model/Cerveja.java
+++ brewer/src/main/java/model/Cerveja.java
@@ -1,8 +1,12 @@
 package model;
 
+import org.hibernate.validator.constraints.NotBlank;
+
 public class Cerveja {
 
+	@NotBlank
 	private String sku;
+	
 	private String nome;
 	
 	public String getSku() {
