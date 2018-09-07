85d61d33296178e89baf78bb3eca378fac4c7914
Hélio Márcio Filho <heliommsfilho@outlook.com>
2018-09-07 12:18:26 -0300

    9.5. Montando os combos e radio do cadastro da cerveja

--- brewer/src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ brewer/src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -1,7 +1,5 @@
 package com.algaworks.brewer.controller;
 
-import java.util.Optional;
-
 import javax.validation.Valid;
 
 import org.slf4j.Logger;
@@ -12,10 +10,13 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.brewer.model.Cerveja;
-import com.algaworks.brewer.repository.Cervejas;
+import com.algaworks.brewer.model.Origem;
+import com.algaworks.brewer.model.Sabor;
+import com.algaworks.brewer.repository.Estilos;
 
 @Controller
 public class CervejasController {
@@ -23,17 +24,20 @@ public class CervejasController {
 	private static final Logger logger = LoggerFactory.getLogger(CervejasController.class);
 
 	@Autowired
-	private Cervejas cervejas;
+	private Estilos estilos;
 	
 	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.GET)
-	public String novo(Cerveja cerveja) {
-		Optional<Cerveja> cervejaOptional = cervejas.findBySku("AA1111");
+	public ModelAndView novo(Cerveja cerveja) {
+		ModelAndView mv = new ModelAndView("cerveja/CadastroCerveja");
+		mv.addObject("sabores", Sabor.values());
+		mv.addObject("estilos", estilos.findAll());
+		mv.addObject("origens", Origem.values());
 		
-		return "cerveja/CadastroCerveja";
+		return mv;
 	}
 	
 	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
-	public String cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
+	public ModelAndView cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
 		
 		if (result.hasErrors()) {
 			return novo(cerveja);
@@ -41,7 +45,7 @@ public class CervejasController {
 		
 		attributes.addFlashAttribute("mensagem", "Cerveja salva com sucesso!");
 		System.out.println("Chamou Cadastrar ---> " + cerveja.getSku());
-		return "redirect:/cervejas/novo";
+		return new ModelAndView("redirect:/cervejas/novo");
 	}
 	
 	@RequestMapping("/cervejas/cadastro")
