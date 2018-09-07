22df37f2d790936526945ebfadf854ed72b8e8c5
Hélio Márcio Filho <heliommsfilho@outlook.com>
2018-09-07 11:12:53 -0300

    9.2. Configurando o projeto

--- brewer/src/main/java/com/algaworks/brewer/config/init/AppInitializer.java
+++ brewer/src/main/java/com/algaworks/brewer/config/init/AppInitializer.java
@@ -5,6 +5,7 @@ import javax.servlet.Filter;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+import com.algaworks.brewer.config.JPAConfig;
 import com.algaworks.brewer.config.WebConfig;
 
 /* Configurações do Front Controller do Spring (DispatcherServlet) */
@@ -12,7 +13,7 @@ public class AppInitializer extends AbstractAnnotationConfigDispatcherServletIni
 
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
-		return null;
+		return new Class<?>[] { JPAConfig.class };
 	}
 
 	/* Ensina o Spring onde achar os Controllers */
--- brewer/src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ brewer/src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -12,14 +13,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.brewer.model.Cerveja;
+import com.algaworks.brewer.repository.Cervejas;
 
 @Controller
 public class CervejasController {
 
 	private static final Logger logger = LoggerFactory.getLogger(CervejasController.class);
+
+	@Autowired
+	private Cervejas cervejas;
 	
 	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.GET)
 	public String novo(Cerveja cerveja) {
+		cervejas.findAll();
 		return "cerveja/CadastroCerveja";
 	}
 	
