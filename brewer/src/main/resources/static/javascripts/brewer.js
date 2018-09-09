ab657d38e925d26b1a5cf47e8845c9a742597938
Hélio Márcio Filho <heliommsfilho@outlook.com>
2018-09-09 11:02:34 -0300

    10.3. Convertendo valores numéricos

--- brewer/src/main/resources/static/javascripts/brewer.js
+++ brewer/src/main/resources/static/javascripts/brewer.js
@@ -1,8 +1,8 @@
 $(function() {
 	
 	var decimal = $('.js-decimal');
-	decimal.maskMoney();
+	decimal.maskMoney({ decimal: ',', thousands: '.' });
 	
 	var plain = $('.js-plain');
-	plain.maskMoney({ precision: 0 });
+	plain.maskMoney({ precision: 0, thousands: '.' });
 });
