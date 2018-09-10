3f124dd7c4f83edcb6a765238aa2a97d4d547689
Hélio Márcio Filho <heliommsfilho@outlook.com>
2018-09-10 20:37:26 -0300

    12.3. Modularizando o cadastro rápido de estilo

--- brewer/src/main/resources/static/javascripts/estilo.cadastro-rapido.js
+++ brewer/src/main/resources/static/javascripts/estilo.cadastro-rapido.js
@@ -3,18 +3,18 @@ var Brewer = Brewer || {};
 Brewer.EstiloCadastroRapido = (function() {
 	
 	function EstiloCadastroRapido() {
-		this.modal = $("#modalCadastroRapidoEstilo");
+		this.modal = $('#modalCadastroRapidoEstilo');
 		this.botaoSalvar = this.modal.find('.js-modal-cadastro-estilo-salvar-btn');
-		this.form = modal.find('form');
+		this.form = this.modal.find('form');
 		this.url = this.form.attr('action');
 		this.inputNomeEstilo = $('#nomeEstilo');
 		this.containerMensagemErro = $('.js-mensagem-cadastro-rapido-estilo');
 	}
 	
 	EstiloCadastroRapido.prototype.iniciar = function() {
-		form.on('submit', function(e) { e.preventDefault() });
+		this.form.on('submit', function(event) { event.preventDefault() });
 		this.modal.on('shown.bs.modal', onModalShow.bind(this));
-		this.modal.on('hide.bs.modal', onModalClose.bind(this));
+		this.modal.on('hide.bs.modal', onModalClose.bind(this))
 		this.botaoSalvar.on('click', onBotaoSalvarClick.bind(this));
 	}
 	
@@ -25,28 +25,27 @@ Brewer.EstiloCadastroRapido = (function() {
 	function onModalClose() {
 		this.inputNomeEstilo.val('');
 		this.containerMensagemErro.addClass('hidden');
-		this.form.find('form-group').removeClass('has-error');
+		this.form.find('.form-group').removeClass('has-error');
 	}
 	
 	function onBotaoSalvarClick() {
 		var nomeEstilo = this.inputNomeEstilo.val().trim();
-		
 		$.ajax({
 			url: this.url,
 			method: 'POST',
 			contentType: 'application/json',
 			data: JSON.stringify({ nome: nomeEstilo }),
-			error: onErrorSalvandoEstilo.bind(this),
+			error: onErroSalvandoEstilo.bind(this),
 			success: onEstiloSalvo.bind(this)
 		});
 	}
 	
-	function onErrorSalvandoEstilo(obj) {
+	function onErroSalvandoEstilo(obj) {
 		var mensagemErro = obj.responseText;
 		this.containerMensagemErro.removeClass('hidden');
 		this.containerMensagemErro.html('<span>' + mensagemErro + '</span>');
 		this.form.find('.form-group').addClass('has-error');
-	} 
+	}
 	
 	function onEstiloSalvo(estilo) {
 		var comboEstilo = $('#estilo');
@@ -56,10 +55,10 @@ Brewer.EstiloCadastroRapido = (function() {
 	}
 	
 	return EstiloCadastroRapido;
+	
 }());
 
 $(function() {
-	
 	var estiloCadastroRapido = new Brewer.EstiloCadastroRapido();
 	estiloCadastroRapido.iniciar();
 });
