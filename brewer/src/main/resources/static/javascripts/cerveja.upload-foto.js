7e7a6cb5d08bfe9da166fbc11b893c117a1bcd25
Hélio Márcio Filho <heliommsfilho@outlook.com>
2018-09-16 18:10:27 -0300

    14.10. Mantendo a foto na validação

--- brewer/src/main/resources/static/javascripts/cerveja.upload-foto.js
+++ brewer/src/main/resources/static/javascripts/cerveja.upload-foto.js
@@ -22,6 +22,10 @@ Brewer.UploadFoto= (function(){
 		
 		UIkit.uploadSelect($('#upload-select'), settings);
 		UIkit.uploadDrop(this.uploadDrop, settings);
+		
+		if (this.inputNomeFoto.val()) {
+			onUploadCompleto.call(this, {nome: this.inputNomeFoto.val(), contentType: this.inputContentType.val()});
+		}
 	}
 	
 	function onUploadCompleto(resposta) {
