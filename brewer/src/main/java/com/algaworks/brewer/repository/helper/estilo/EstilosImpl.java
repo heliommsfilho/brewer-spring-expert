c4355f39bdf99d46461c12a99216c2cf59d0b871
Hélio Márcio Filho <heliommsfilho@outlook.com>
2018-10-25 20:06:52 -0300

    15.18. Bean útil para paginação

--- brewer/src/main/java/com/algaworks/brewer/repository/helper/estilo/EstilosImpl.java
+++ brewer/src/main/java/com/algaworks/brewer/repository/helper/estilo/EstilosImpl.java
@@ -6,23 +6,26 @@ import javax.persistence.PersistenceContext;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.criterion.MatchMode;
-import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
 import com.algaworks.brewer.model.Estilo;
 import com.algaworks.brewer.repository.filter.EstiloFilter;
+import com.algaworks.brewer.repository.paginacao.PaginacaoUtil;
 
 public class EstilosImpl implements EstilosQueries {
 
 	@PersistenceContext
 	private EntityManager manager;
+	
+	@Autowired
+	private PaginacaoUtil PaginacaoUtil;
 	
 	@Transactional(readOnly = true)
 	@SuppressWarnings("unchecked")
@@ -30,21 +33,7 @@ public class EstilosImpl implements EstilosQueries {
 	public Page<Estilo> filtrar(EstiloFilter filter, Pageable pageable) {
 		Criteria criteria = manager.unwrap(Session.class).createCriteria(Estilo.class);
 		
-		int totalRegistrosPorPagina = pageable.getPageSize();
-		int paginaAtual = pageable.getPageNumber();
-		int primeiroRegistro = paginaAtual * totalRegistrosPorPagina;
-		
-		criteria.setFirstResult(primeiroRegistro);
-		criteria.setMaxResults(totalRegistrosPorPagina);
-		
-		Sort sort = pageable.getSort();
-		
-		if (sort != null) {
-			Sort.Order order = sort.iterator().next();
-			String field = order.getProperty();
-			criteria.addOrder(order.isAscending() ? Order.asc(field) : Order.desc(field));
-		}
-		
+		PaginacaoUtil.preparar(criteria, pageable);
 		adicionarFiltro(filter, criteria);
 		
 		return new PageImpl<>(criteria.list(), pageable, total(filter));
