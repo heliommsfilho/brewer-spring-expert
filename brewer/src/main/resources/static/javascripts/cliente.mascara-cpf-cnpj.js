a01859fe32870b73cce4bb92a0465abea24d67bb
Hélio Márcio Filho <heliommsfilho@outlook.com>
2018-11-07 19:46:18 -0300

    16.3. Configurando máscara de CPF/CNPJ com jQuery

---
+++ brewer/src/main/resources/static/javascripts/cliente.mascara-cpf-cnpj.js
@@ -0,0 +1,29 @@
+var Brewer = Brewer || {};
+
+Brewer.MascaraCpfCnpj = (function() {
+	
+	function MascaraCpfCnpj() {
+		this.radioTipoPessoa = $('.js-radio-tipo-pessoa');
+		this.labelCpfCnpj = $('[for=cpfOuCnpj]');
+		this.inputCpfCnpj = $('#cpfOuCnpj');
+	}
+	
+	MascaraCpfCnpj.prototype.iniciar = function() {
+		this.radioTipoPessoa.on('change', onTipoPessoaAlterado.bind(this));
+	}
+	
+	function onTipoPessoaAlterado(evento) {
+		var tipoPessoaSelecionada = $(evento.currentTarget);
+		this.labelCpfCnpj.text(tipoPessoaSelecionada.data('documento'));
+		this.inputCpfCnpj.mask(tipoPessoaSelecionada.data('mascara'));
+		this.inputCpfCnpj.val('');
+		this.inputCpfCnpj.removeAttr('disabled');
+	}
+	
+	return MascaraCpfCnpj;
+}());
+
+$(function() {
+	var MascaraCpfCnpj = new Brewer.MascaraCpfCnpj();
+	MascaraCpfCnpj.iniciar();
+});
