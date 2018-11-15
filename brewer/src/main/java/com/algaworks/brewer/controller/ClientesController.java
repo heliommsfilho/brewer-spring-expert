ff408bcc5984a65effb8052509a6b80aaddee729
Hélio Márcio Filho <heliommsfilho@outlook.com>
2018-11-15 16:27:50 -0300

    16.12. Desafio: Começando a pesquisa de clientes

--- brewer/src/main/java/com/algaworks/brewer/controller/ClientesController.java
+++ brewer/src/main/java/com/algaworks/brewer/controller/ClientesController.java
@@ -1,18 +1,25 @@
 package com.algaworks.brewer.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.algaworks.brewer.controller.page.PageWrapper;
 import com.algaworks.brewer.model.Cliente;
 import com.algaworks.brewer.model.TipoPessoa;
+import com.algaworks.brewer.repository.Clientes;
 import com.algaworks.brewer.repository.Estados;
+import com.algaworks.brewer.repository.filter.ClienteFilter;
 import com.algaworks.brewer.service.CadastroClienteService;
 import com.algaworks.brewer.service.exception.CpfCnpjClienteJaCadastradoException;
 
@@ -22,6 +29,9 @@ public class ClientesController {
 	
 	@Autowired
 	private Estados estados;
+	
+	@Autowired
+	private Clientes clientes;
 	
 	@Autowired
 	private CadastroClienteService cadastroClienteService;
@@ -50,5 +60,17 @@ public class ClientesController {
 		}
 		attributes.addFlashAttribute("mensagem", "Cliente salvo com sucesso!");
 		return new ModelAndView("redirect:/clientes/novo");
+	}
+	
+	@GetMapping
+	public ModelAndView pesquisar(ClienteFilter clienteFilter, BindingResult result, 
+																@PageableDefault(size = 2) Pageable pageable,
+																HttpServletRequest httpServletRequest) {
+		ModelAndView mv = new ModelAndView("cliente/PesquisaClientes");
+		
+		PageWrapper<Cliente> paginaWrapper = new PageWrapper<>(clientes.filtrar(clienteFilter, pageable), httpServletRequest);
+		mv.addObject("pagina", paginaWrapper);
+		
+		return mv;
 	}
 }
