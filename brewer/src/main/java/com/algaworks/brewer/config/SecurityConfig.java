ec38d7e30523b041674458b21bec8fdbc1d68065
Hélio Márcio Filho <heliommsfilho@outlook.com>
2018-11-25 10:51:14 -0300

    19.5. Restringindo o acesso às páginas

--- brewer/src/main/java/com/algaworks/brewer/config/SecurityConfig.java
+++ brewer/src/main/java/com/algaworks/brewer/config/SecurityConfig.java
@@ -36,8 +36,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-				.anyRequest()
-					.authenticated()
+				.antMatchers("cidades/nova").hasRole("CADASTRAR_CIDADE")
+				.antMatchers("usuarios/**").hasRole("CADASTRAR_USUARIO")
+				.anyRequest().authenticated()
 			.and()
 				.formLogin()
 					.loginPage("/login")
