55f03863a6002ba406f777802a51386a7f8cbdaf
Hélio Márcio Filho <heliommsfilho@outlook.com>
2018-12-07 09:42:36 -0300

    21.3. Trabalhando no filtro por grupo

--- brewer/src/main/java/com/algaworks/brewer/repository/helper/usuario/UsuariosImpl.java
+++ brewer/src/main/java/com/algaworks/brewer/repository/helper/usuario/UsuariosImpl.java
@@ -1,12 +1,28 @@
 package com.algaworks.brewer.repository.helper.usuario;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
+import org.hibernate.criterion.Subqueries;
+import org.hibernate.sql.JoinType;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
+
+import com.algaworks.brewer.model.Grupo;
 import com.algaworks.brewer.model.Usuario;
+import com.algaworks.brewer.model.UsuarioGrupo;
+import com.algaworks.brewer.repository.filter.UsuarioFilter;
 
 public class UsuariosImpl implements UsuariosQueries {
 
@@ -20,6 +36,18 @@ public class UsuariosImpl implements UsuariosQueries {
 				.setParameter("email", email)
 				.getResultList().stream().findFirst();
 	}
+	
+	@SuppressWarnings("unchecked")
+	@Transactional(readOnly = true)
+	@Override
+	public List<Usuario> filtrar(UsuarioFilter filtro) {
+		Criteria criteria = manager.unwrap(Session.class).createCriteria(Usuario.class);
+		
+		criteria.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
+		adicionarFiltro(filtro, criteria);
+		
+		return criteria.list();
+	}
 
 	@Override
 	public List<String> permissoes(Usuario usuario) {
@@ -28,5 +56,34 @@ public class UsuariosImpl implements UsuariosQueries {
 				.setParameter("usuario", usuario)
 				.getResultList();
 	}
-
+	
+	private void adicionarFiltro(UsuarioFilter filtro, Criteria criteria) {
+		
+		if (filtro != null) {
+			
+			if (!StringUtils.isEmpty(filtro.getNome())) {
+				criteria.add(Restrictions.ilike("nome", filtro.getNome(), MatchMode.ANYWHERE));
+			}
+			
+			if (!StringUtils.isEmpty(filtro.getEmail())) {
+				criteria.add(Restrictions.ilike("email", filtro.getEmail(), MatchMode.START));
+			}
+			
+			criteria.createAlias("grupos", "g", JoinType.LEFT_OUTER_JOIN);
+			
+			if (filtro.getGrupos() != null && !filtro.getGrupos().isEmpty()) {
+				List<Criterion> subQueries = new ArrayList<>();
+				
+				for(Long codigoGrupo : filtro.getGrupos().stream().mapToLong(Grupo::getCodigo).toArray()) {
+					DetachedCriteria dc = DetachedCriteria.forClass(UsuarioGrupo.class);
+					dc.add(Restrictions.eq("id.grupo.codigo", codigoGrupo));
+					dc.setProjection(Projections.property("id.usuario"));
+					
+					subQueries.add(Subqueries.propertyIn("codigo", dc));
+				}
+				Criterion[] criterions = new Criterion[subQueries.size()];
+				criteria.add(Restrictions.and(subQueries.toArray(criterions)));
+			}
+		}
+	}
 }
--- brewer/src/main/java/com/algaworks/brewer/repository/helper/usuario/UsuariosQueries.java
+++ brewer/src/main/java/com/algaworks/brewer/repository/helper/usuario/UsuariosQueries.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import com.algaworks.brewer.model.Usuario;
+import com.algaworks.brewer.repository.filter.UsuarioFilter;
 
 public interface UsuariosQueries {
 
 	Optional<Usuario> usuarioEmailAtivo(String email);
 	List<String> permissoes(Usuario usuario);
+	List<Usuario> filtrar(UsuarioFilter filtro);
 }
