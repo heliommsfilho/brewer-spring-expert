5226c3be6dbfbbe551f3862c2c67d870a461a507
Hélio Márcio Filho <heliommsfilho@outlook.com>
2018-12-09 12:04:59 -0300

    21.6. Paginação da pesquisa de usuários

--- brewer/src/main/java/com/algaworks/brewer/repository/helper/usuario/UsuariosQueries.java
+++ brewer/src/main/java/com/algaworks/brewer/repository/helper/usuario/UsuariosQueries.java
@@ -3,6 +3,9 @@ package com.algaworks.brewer.repository.helper.usuario;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.algaworks.brewer.model.Usuario;
 import com.algaworks.brewer.repository.filter.UsuarioFilter;
 
@@ -10,5 +13,5 @@ public interface UsuariosQueries {
 
 	Optional<Usuario> usuarioEmailAtivo(String email);
 	List<String> permissoes(Usuario usuario);
-	List<Usuario> filtrar(UsuarioFilter filtro);
+	Page<Usuario> filtrar(UsuarioFilter filtro, Pageable pageable);
 }
