f7a7ddf975b16643a6c7277a1832443eab6aee9e
Hélio Márcio Filho <heliommsfilho@outlook.com>
2018-12-10 10:47:08 -0300

    22.6. Pesquisando cervejas por sku ou nome

--- brewer/src/main/java/com/algaworks/brewer/repository/helper/cerveja/CervejasQueries.java
+++ brewer/src/main/java/com/algaworks/brewer/repository/helper/cerveja/CervejasQueries.java
@@ -1,12 +1,16 @@
 package com.algaworks.brewer.repository.helper.cerveja;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import com.algaworks.brewer.dto.CervejaDTO;
 import com.algaworks.brewer.model.Cerveja;
 import com.algaworks.brewer.repository.filter.CervejaFilter;
 
 public interface CervejasQueries {
 
-	public Page<Cerveja> filtrar(CervejaFilter filtro, Pageable pageable);
+	Page<Cerveja> filtrar(CervejaFilter filtro, Pageable pageable);
+	List<CervejaDTO> porSkuOuNome(String skuOuNome); 
 }
