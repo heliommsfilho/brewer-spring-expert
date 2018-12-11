e928eeb605dfe9ab8ab0a1e9da7de041f7a6b05d
Hélio Márcio Filho <heliommsfilho@outlook.com>
2018-12-10 22:51:37 -0300

    23.3. Criando tabela de itens de venda

---
+++ brewer/src/main/java/com/algaworks/brewer/model/ItemVenda.java
@@ -0,0 +1,69 @@
+package com.algaworks.brewer.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+public class ItemVenda implements Serializable {
+
+	private static final long serialVersionUID = -3795575927886450047L;
+	
+	private Long codigo;
+	private Integer quantidade;
+	private BigDecimal valorUnitario;
+	private Cerveja cerveja;
+	
+	public Long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+	public Cerveja getCerveja() {
+		return cerveja;
+	}
+	public void setCerveja(Cerveja cerveja) {
+		this.cerveja = cerveja;
+	}
+	
+	public BigDecimal getValorTotal() {
+		return valorUnitario.multiply(new BigDecimal(quantidade));
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		result = prime * result + ((valorUnitario == null) ? 0 : valorUnitario.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ItemVenda other = (ItemVenda) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		if (valorUnitario == null) {
+			if (other.valorUnitario != null)
+				return false;
+		} else if (!valorUnitario.equals(other.valorUnitario))
+			return false;
+		return true;
+	}
+
+}
