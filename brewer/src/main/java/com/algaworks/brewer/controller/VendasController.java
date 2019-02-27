dc6e8f46a00c597f50c82a3c39ab502dbf1f7490
Hélio Márcio Filho <heliommsfilho@outlook.com>
2019-02-27 21:53:17 +0000

    24.7. Formatando valores numéricos no e-mail

--- brewer/src/main/java/com/algaworks/brewer/controller/VendasController.java
+++ brewer/src/main/java/com/algaworks/brewer/controller/VendasController.java
@@ -118,10 +118,10 @@ public class VendasController {
 		}
 		venda.setUsuario(usuarioSistema.getUsuario());
 		
-		cadastroVendaService.salvar(venda);
+		venda = cadastroVendaService.salvar(venda);
 		mailer.enviar(venda);
 		
-		attributes.addFlashAttribute("mensagem", "Venda salva e e-mail enviado");
+		attributes.addFlashAttribute("mensagem", String.format("Venda nº %d salva com sucesso e e-mail enviado", venda.getCodigo()));
 		return new ModelAndView("redirect:/vendas/nova");
 	}
 	
--- brewer/src/main/java/com/algaworks/brewer/mail/Mailer.java
+++ brewer/src/main/java/com/algaworks/brewer/mail/Mailer.java
@@ -1,6 +1,7 @@
 package com.algaworks.brewer.mail;
 
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 import javax.mail.MessagingException;
@@ -39,7 +40,8 @@ public class Mailer {
 	
 	@Async
 	public void enviar(Venda venda) {
-		Context context = new Context();
+		Context context = new Context(new Locale("pt", "BR"));
+		
 		context.setVariable("venda", venda);
 		context.setVariable("logo", "logo");
 		
@@ -67,7 +69,7 @@ public class Mailer {
 			MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
 			helper.setFrom("heliommsfilho@gmail.com");
 			helper.setTo(venda.getCliente().getEmail());
-			helper.setSubject("Brewer - Venda realizada");
+			helper.setSubject(String.format("Brewer - Venda nº %d", venda.getCodigo()));
 			helper.setText(email, true);
 			
 			helper.addInline("logo", new ClassPathResource("static/images/logo-gray.png"));
