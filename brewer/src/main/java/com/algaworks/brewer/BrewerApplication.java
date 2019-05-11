2db71a7d9eaf4bc978f9565c49049e52498d2a79
Hélio Márcio Filho <heliommsfilho@protonmail.com>
2019-05-11 18:26:06 +0100

    32.11. Ajuste na classe CervejaEntityListener

--- brewer/src/main/java/com/algaworks/brewer/BrewerApplication.java
+++ brewer/src/main/java/com/algaworks/brewer/BrewerApplication.java
@@ -2,11 +2,18 @@ package com.algaworks.brewer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class BrewerApplication {
 
+	private static ApplicationContext applicationContext;
+	
 	public static void main(String[] args) {
-		SpringApplication.run(BrewerApplication.class, args);
+		applicationContext = SpringApplication.run(BrewerApplication.class, args);
+	}
+	
+	public static <T> T getBean(Class<T> requiredType) {
+		return applicationContext.getBean(requiredType);
 	}
 }
--- brewer/src/main/java/com/algaworks/brewer/repository/listener/CervejaEntityListener.java
+++ brewer/src/main/java/com/algaworks/brewer/repository/listener/CervejaEntityListener.java
@@ -2,20 +2,15 @@ package com.algaworks.brewer.repository.listener;
 
 import javax.persistence.PostLoad;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.context.support.SpringBeanAutowiringSupport;
-
+import com.algaworks.brewer.BrewerApplication;
 import com.algaworks.brewer.model.Cerveja;
 import com.algaworks.brewer.storage.FotoStorage;
 
 public class CervejaEntityListener {
-
-	@Autowired
-	private FotoStorage fotoStorage;
 	
 	@PostLoad
-	public void postLoad(final Cerveja cerveja) {
-		SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
+	public void postLoad(final Cerveja cerveja) {	
+		FotoStorage fotoStorage = BrewerApplication.getBean(FotoStorage.class);
 		
 		cerveja.setUrlFoto(fotoStorage.getUrl(cerveja.getFotoOuMock()));
 		cerveja.setUrlThumbnailFoto(fotoStorage.getUrl(FotoStorage.THUMBNAIL_PERFIX + cerveja.getFotoOuMock()));
